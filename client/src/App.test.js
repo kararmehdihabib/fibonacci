1b15ab57e5fa1976dfb10f83c5ee7b3377af3a1a
KARAR MEHDI HABIB <karar.habib@castor.fi>
2019-03-12 13:38:19 +0200

    test suite configured

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-	console.log("passed");
-});
+it('renders without crashing', () => {});
